478e35de2e06649604fb37a84791e84769fe65e3
hjseo <mijin2197@hanmail.net>
2021-07-13 23:17:46 +0900

    hoMEnu 프로젝트 생성

---
+++ src/main/java/com/hj/hoMEnu/HoMEnuApplication.java
@@ -0,0 +1,13 @@
+package com.hj.hoMEnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoMEnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoMEnuApplication.class, args);
+	}
+
+}
